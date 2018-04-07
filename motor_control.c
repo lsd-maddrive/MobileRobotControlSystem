30464fb2afcf290d9d799ba3b3ae354cad0dd54c
Dima <PonomarevDA96@gmail.com>
2018-04-07 14:10:20 +0300

    fix motor_control :: motor_init() - add pwm3 and pwm4

--- motor_control.c
+++ motor_control.c
@@ -22,17 +22,21 @@ void motor_init()
     {
         CLOCK_FREQUENCY = 16000000,
         PWM_FREQUENCY = 16000,
-        PRESCALER = 1,
-        PWM_PERIOD = CLOCK_FREQUENCY/( PWM_FREQUENCY * PRESCALER) - 1  // max 32767
+        PRM_PRESCALER = 1,
+        PWM_PERIOD = CLOCK_FREQUENCY/( PWM_FREQUENCY * PRM_PRESCALER) - 1  // max 32767
     };
-    P1TCONbits.PTCKPS = 0x00;   // PWM time base input clock period is TCY (1:1 prescale)
-    P1TCONbits.PTMOD = 0x00;    // PWM time base operates in Free Running mode
+    P1TCONbits.PTCKPS = 0x00;   // PWM PRESCALER (1:1 prescale)
+    P1TCONbits.PTMOD = 0x00;    // PWM MODE: Free Running mode
     P1TMRbits.PTMR = 0;         // PWM time base is counting up
     
     PWM1CON1bits.PMOD1 = 1;     // PWM1 I/O pin pair is in Independent Output mode
     PWM1CON1bits.PEN1L = 1;     // PWM1 pin is enabled for PWM output
     PWM1CON1bits.PMOD2 = 1;     // PWM2 I/O pin pair is in Independent Output mode
     PWM1CON1bits.PEN2L = 1;     // PWM2 pin is enabled for PWM output
+    PWM1CON1bits.PMOD1 = 1;     // PWM3 I/O pin pair is in Independent Output mode
+    PWM1CON1bits.PEN1L = 1;     // PWM3 pin is enabled for PWM output
+    PWM1CON1bits.PMOD2 = 1;     // PWM4 I/O pin pair is in Independent Output mode
+    PWM1CON1bits.PEN2L = 1;     // PWM4 pin is enabled for PWM output
     
     P1TPERbits.PTPER = PWM_PERIOD;//PWM Time Base Period Value bits (max 32676)
     
